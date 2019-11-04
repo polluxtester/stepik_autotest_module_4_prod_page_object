1194943f2be0a1ded707f6a0f6d6c150ac611ade
ptt <pollux.the.tester@gmail.com>
2019-11-04 13:40:57 +0300

    Move test test_user_can_add_product_to_basket back to class

--- test_product_page.py
+++ test_product_page.py
@@ -5,16 +5,6 @@ import pytest
 import time
 
 @pytest.mark.need_review
-def test_user_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_cart()
-    page.is_added_to_basket_item_correct()
-    page.is_price_correct()
-
-@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -83,6 +73,15 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_cart()
+        page.is_added_to_basket_item_correct()
+        page.is_price_correct()
+	
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
